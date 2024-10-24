2ed1304e6a5bec32b5ec55df8133df4299803a4a
Ish Sarin <sarinish2000@gmail.com>
2024-10-25 04:17:06 +0530

    Update MobileNavBar.jsx

--- my-portfolio/src/components/MobileNavBar.jsx
+++ my-portfolio/src/components/MobileNavBar.jsx
@@ -137,8 +137,8 @@ export const MobileNavBar = () => {
         <button>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="25"
+            height="25"
             fill="currentColor"
             class="bi bi-arrow-up"
             viewBox="0 0 16 16"
